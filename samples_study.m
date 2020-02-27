4baf2e668b0565e6309e52ca58f1e93a533a800b
kayuela <56114643+kayuela@users.noreply.github.com>
2020-02-27 20:06:54 +0100

    Samples script finished

--- samples_study.m
+++ samples_study.m
@@ -3,7 +3,7 @@
 path="C:\Users\ffran\Dropbox\TFG\Statistical-Study\Sample files\Escenarios\Pruebas\25-02-2020 171914-samples.txt";
 samples=csvread(path);
 
-%% Calculating statisctical moments
+%% Calculating statisctical moments & Saving results & figures
 
 %Deviation parameters
 deviations=samples(:,4);
@@ -22,38 +22,33 @@ figure(1);
 dist_histogram=histogram(distances,12);
 title("Distance histogram");
 xlabel("N=2000");
+saveas(gcf,"distance_histogram.png");
 
 %Deviation histogram
 figure(2);
 dev_histogram=histogram(deviations,12);
 title("Deviation histogram");
 xlabel("N=2000");
+saveas(gcf,'deviation_histogram.png');
 
 %Distance PDF estimation
 figure(3);
 dist_pdf_estimation=ksdensity(distances);
 plot(dist_pdf_estimation);
 xlabel("Distance PDF estimation");
+saveas(gcf,'distance_pdf_estimation.png');
 
 %Deviation PDF estimation
 figure(4);
 dev_pdf_estimation=ksdensity(deviations);
 plot(dev_pdf_estimation);
 xlabel("Deviation PDF estimation");
+saveas(gcf,'deviation_pdf_estimation.png');
 
-%% Saving results & figures
-
-path="C:\Users\ffran\Dropbox\TFG\Statistical-Study\Output files\Escenarios\Pruebas\salida_prueba.txt.txt";
+path="C:\Users\ffran\Dropbox\TFG\Statistical-Study\salida_prueba.txt.txt";
 fileID=fopen(path,'w');
 
-real_distance=mean(samples(:,3));
+real_distance=mean(samples(:,2));
 fprintf(fileID,"%f,%f,%f",real_distance,distanceMean,distanceDeviation);
 
-bar(dist_histogram);
-saveas(gcf,'distance_histogram.png');
-bar(dev_histogram);git add
-saveas(gcf,'deviation_histogram.png');
-bar(dist_pdf_estimation);
-saveas(gcf,'dist_pdf_estimation.png');
-bar(dev_pdf_estimation);
-saveas(gcf,'dev_pdf_estimation.png');
+fclose(fileID);
