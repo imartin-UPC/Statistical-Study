1da81f80c634337f0dec297286aae1c899a99f78
kayuela <56114643+kayuela@users.noreply.github.com>
2020-03-01 14:01:00 +0100

    Added CI as a parameter to calculate in both functions

--- samples_study.m
+++ samples_study.m
@@ -1,17 +1,21 @@
 function [real_distance,distanceMean,distanceDeviation,distanceMedian] = samples_study(dir_out,filename_output,samples)
 % Calculating statisctical moments
+% Distance parameters
+distances=samples(:,3);
+vNormal=fitdist(distances,'Normal');
+normalCI=paramci(vNormal);
+distanceCI=normalCI(:,1);
+distanceMean=mean(distances);
+distanceMedian=median(distances);
+distanceDeviation=std(distances);
+real_distance=mean(samples(:,2));
+
 % Deviation parameters
 deviations=samples(:,4);
 deviationMean=mean(deviations);
 deviationMedian=median(deviations);
 deviationDeviation=std(deviations);
-
-% Distance parameters
-distances=samples(:,3);
-distanceMean=mean(distances);
-distanceMedian=median(distances);
-distanceDeviation=std(distances);
-real_distance=mean(samples(:,2));
+deviationCI=normalCI(:,2);
 
 % Plotting desired graphs & Saving results & figures
 % Distance histogram
@@ -45,6 +49,6 @@ saveas(gcf,dir_out+'deviation_pdf_estimation.png');
 % Saving information
 path_out=dir_out+filename_output;
 fileID=fopen(path_out,'w');
-fprintf(fileID,"%f,%f,%f,%f",real_distance,distanceMean,distanceDeviation,distanceMedian);
+fprintf(fileID,"%f,%f,%f,%f,%f,%f,%f,%f",real_distance,distanceMean,distanceCI(1),distanceCI(2),distanceMedian,distanceDeviation,deviationCI(1),deviationCI(2));
 fclose(fileID);
 end
