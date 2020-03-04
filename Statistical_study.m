f75c6abdfc06152cc2e989726009e58871331b00
kayuela <56114643+kayuela@users.noreply.github.com>
2020-03-04 21:21:25 +0100

    Testing battery made

--- Statistical_study.m
+++ Statistical_study.m
@@ -1,17 +1,18 @@
 %% Samples Statistical Study
 % Loading the file
-path="C:\Users\ffran\Dropbox\TFG\Statistical-Study\prueba_input\25-02-2020 171914-samples.txt";
+path="C:\Users\ffran\Dropbox\TFG\Statistical-Study\Sample files\Batería de prueba\Google\20m\03-03-2020 161752-samples.txt";
 samples=csvread(path);
 
 % Calling samples_study function
-dir_out="C:\Users\ffran\Dropbox\TFG\Statistical-Study\prueba_output_samples\";
-[real_distance,distanceMean,distanceDeviation,distanceMedian] = samples_study(dir_out,"salida_samples_prueba.txt",samples);
+dir_out="C:\Users\ffran\Dropbox\TFG\Statistical-Study\Output files\Batería de prueba\Google\20m\";
+[real_distance,distanceMean,distanceDeviation,distanceMedian] = samples_study(dir_out,"samples_output.txt",samples);
 
 %% Batches Statistical Study
 % Loading the file
-path="C:\Users\ffran\Dropbox\TFG\Statistical-Study\prueba_input\25-02-2020 193225-batches.txt";
+path="C:\Users\ffran\Dropbox\TFG\Statistical-Study\Sample files\Batería de prueba\Google\10m\03-03-2020 153720-batches.txt";
 batches=readtable(path);
+fieldnames(batches)
 
 % Calling batches_study function
-dir_out="C:\Users\ffran\Dropbox\TFG\Statistical-Study\prueba_output_batches\";
-[mean_att_samples,mean_succ_samples,BSSID,succ_ratio,path_out]=batches_study(dir_out,"salida_batches_prueba.txt",batches);
+dir_out="C:\Users\ffran\Dropbox\TFG\Statistical-Study\Output files\Batería de prueba\Google\10m\";
+[mean_att_samples,mean_succ_samples,BSSID,succ_ratio,path_out]=batches_study(dir_out,"batches_output.txt",batches);
