83cb37bdc19a277f83c1fb1505bde42745c944ba
kayuela <56114643+kayuela@users.noreply.github.com>
2020-03-14 12:46:53 +0100

    Fixed batches_study, now working

--- Statistical_study.m
+++ Statistical_study.m
@@ -10,8 +10,8 @@ dir_out="C:\Users\ffran\Dropbox\TFG\Statistical-Study\Output files\Batería de pr
 %% Batches Statistical Study
 % Loading the file
 path="C:\Users\ffran\Dropbox\TFG\Statistical-Study\Sample files\Batería de prueba\Google\10m\03-03-2020 153720-batches.txt";
-batches=readtable(path);
+batches=load(path);
 
 % Calling batches_study function
 dir_out="C:\Users\ffran\Dropbox\TFG\Statistical-Study\Output files\Batería de prueba\Google\10m\";
-[mean_att_samples,mean_succ_samples,BSSID,succ_ratio,path_out]=batches_study(dir_out,"batches_output.txt",batches);
+[mean_att_samples,mean_succ_samples,succ_ratio,path_out]=batches_study(dir_out,"batches_output.txt",batches);
--- batches_study.m
+++ batches_study.m
@@ -1,42 +1,40 @@
-function [mean_att_samples,mean_succ_samples,BSSID,succ_ratio,path_out] = batches_study(dir_out,filename_output,batches)
+function [mean_att_samples,mean_succ_samples,succ_ratio,path_out] = batches_study(dir_out,filename_output,batches)
 % Important information gathering & calculating
 %Success Ratio Calculation
-successful_samples_vector=batches(:,3);
-attempted_samples_vector=batches(:,4);
-succ_ratio=mean(successful_samples_vector.Var3)/mean(attempted_samples_vector.Var4);
-
-%Getting the BSSID of the AP
-mac=cell2mat(batches(:,1).Var1(1));
-BSSID=string(mac);
+successful_samples=batches(:,3);
+attempted_samples=batches(:,4);
 
 % Output Information - Attempted samples
-attempted_samples=attempted_samples_vector.Var4;
 mean_att_samples=mean(attempted_samples);
 dev_att_samples=std(attempted_samples);
 
 % Output Information - Successful samples
-successful_samples=successful_samples_vector.Var3;
 mean_succ_samples=mean(successful_samples);
 dev_succ_samples=std(successful_samples);
 
-% CI calculating
-vNormalSucc=fitdist(successful_samples,'Normal');
-normalSuccCI=paramci(vNormalSucc);
-SuccCI=normalSuccCI(:,1);
-vNormalAtt=fitdist(attempted_samples,'Normal');
-normalAttCI=paramci(vNormalAtt);
-AttCI=normalAttCI(:,1);
+% Output Information - Success ratio (%)
+succ_ratio=mean_succ_samples/mean_att_samples;
+
+% CI calculating (Not working because we are making just 1 batch per calculation, 
+% so its impossible to fit the data to a Normal variable)
+%vNormalSucc=fitdist(successful_samples,'Normal');
+%normalSuccCI=paramci(vNormalSucc);
+%SuccCI=normalSuccCI(:,1);
+%vNormalAtt=fitdist(attempted_samples,'Normal');
+%normalAttCI=paramci(vNormalAtt);
+%AttCI=normalAttCI(:,1);
 
 % Saving figures 
 histogram(attempted_samples - successful_samples,12);
 title("Failed samples histogram");
 xlabel("Attempted - Successful samples");
 saveas(gcf,dir_out+'failed_samples_histogram.png');
+savefig(gcf,dir_out+'failed_samples_histogram.fig');
 
 % Saving Information gathered
 path_out=dir_out+filename_output;
 fileID=fopen(path_out,'w');
-fprintf(fileID,"%f,%f,%f,%f,%f,%f,%f,%f,%s,%f",mean_att_samples,AttCI(1),AttCI(2),dev_att_samples,mean_succ_samples,SuccCI(1),SuccCI(2),dev_succ_samples,BSSID,succ_ratio);
+fprintf(fileID,"%f,%f,%f,%f,%f",mean_att_samples,dev_att_samples,mean_succ_samples,dev_succ_samples,succ_ratio);
 fclose(fileID);
 end
 
