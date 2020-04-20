11462233e7d988be732f6be926451c5333e125e9
kayuela <56114643+kayuela@users.noreply.github.com>
2020-04-20 20:15:42 +0200

    Applied the scripts to all the sample files and extracted the outputs

--- Statistical_study.m
+++ Statistical_study.m
@@ -1,17 +1,17 @@
 %% Samples Statistical Study
 % Loading the file
-path="C:\Users\ffran\Dropbox\TFG\Statistical-Study\Sample files\Batería de prueba\Google\1m\03-03-2020 143830-samples.txt";
+path="C:\Users\ffran\Dropbox\TFG\Statistical-Study\Sample files\Escenarios\Linksys\5G\5.5G\Interior Pasillo\Orientacion Vertical\23m\11-03-2020 162330-samples.txt";
 samples=csvread(path);
 
 % Calling samples_study function
-dir_out="C:\Users\ffran\Dropbox\TFG\Statistical-Study\Output files\Batería de prueba\Google\1m\";
+dir_out="C:\Users\ffran\Dropbox\TFG\Statistical-Study\Output files\Escenarios\Linksys\5G\5.5G\Interior Pasillo\Orientacion Vertical\23m\";
 [real_distance,distanceMean,distanceDeviation,distanceMedian] = samples_study(dir_out,"samples_output.txt",samples);
 
 %% Batches Statistical Study
 % Loading the file
-path="C:\Users\ffran\Dropbox\TFG\Statistical-Study\Sample files\Batería de prueba\Google\10m\03-03-2020 153720-batches.txt";
+path="C:\Users\ffran\Dropbox\TFG\Statistical-Study\Sample files\Escenarios\Linksys\5G\5.5G\Interior Pasillo\Orientacion Vertical\23m\11-03-2020 162330-batches.txt";
 batches=load(path);
 
 % Calling batches_study function
-dir_out="C:\Users\ffran\Dropbox\TFG\Statistical-Study\Output files\Batería de prueba\Google\10m\";
+dir_out="C:\Users\ffran\Dropbox\TFG\Statistical-Study\Output files\Escenarios\Linksys\5G\5.5G\Interior Pasillo\Orientacion Vertical\23m\";
 [mean_att_samples,mean_succ_samples,succ_ratio,path_out]=batches_study(dir_out,"batches_output.txt",batches);
